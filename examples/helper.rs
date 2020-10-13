ce86068d1ed85bbbe7a213e8446801574146dc21
ngtkana <ngtkana@gmail.com>
2020-10-14 03:41:45 +0900

    Add vector to exampls

--- examples/helper.rs
+++ examples/helper.rs
@@ -12,6 +12,66 @@ pub mod helpers {
     pub struct Key<T>(PhantomData<T>);
 }
 
+pub mod queries {
+    use query_test::{query, Query};
+    use std::marker::PhantomData;
+
+    #[query(fn(usize) -> T)]
+    pub struct Get<T>(PhantomData<T>);
+
+    #[query(fn(usize, T))]
+    pub struct Set<T>(PhantomData<T>);
+}
+
+pub mod vector {
+    #[derive(Debug, Clone, PartialEq)]
+    pub struct Vector<T>(Vec<T>);
+
+    mod vector_impl_gen {
+        use super::{
+            super::{
+                helpers::Value,
+                queries::{Get, Set},
+            },
+            Vector,
+        };
+        use query_test::{Gen, Help};
+        use rand::Rng;
+
+        impl<T, G: Help<Value<T>>> Gen<Get<T>, G> for Vector<T> {
+            fn gen(&self, rng: &mut impl Rng) -> usize {
+                rng.gen_range(0, self.0.len())
+            }
+        }
+
+        impl<T, G: Help<Value<T>>> Gen<Set<T>, G> for Vector<T> {
+            fn gen(&self, rng: &mut impl Rng) -> (usize, T) {
+                (rng.gen_range(0, self.0.len()), G::help(rng))
+            }
+        }
+    }
+
+    mod vector_impl_solve {
+        use super::{
+            super::queries::{Get, Set},
+            Vector,
+        };
+        use query_test::solve::{Mutate, Solve};
+
+        impl<T: Clone> Solve<Get<T>> for Vector<T> {
+            fn solve(&self, i: usize) -> T {
+                self.0[i].clone()
+            }
+        }
+
+        impl<T> Mutate<Set<T>> for Vector<T> {
+            fn mutate(&mut self, (i, x): (usize, T)) {
+                self.0[i] = x;
+            }
+        }
+    }
+}
+
 pub mod gen_example {
     use super::helpers;
     use query_test::Help;
--- examples/queries.rs
+++
@@ -1,5 +0,0 @@
-use query_test::{query, Query};
-use std::marker::PhantomData;
-
-#[query(fn(usize) -> T)]
-pub struct Get<T>(PhantomData<T>);
